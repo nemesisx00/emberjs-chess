c96891475ad79be65c5e4a87764cbed5bb6b302c
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2019-01-11 19:38:22 -0500

    Added a production rootURL since this is will ultimately be embedded within my website.

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+	// here you can enable a production-specific feature
+	ENV.rootURL = 'chess/'
   }
 
   return ENV;
